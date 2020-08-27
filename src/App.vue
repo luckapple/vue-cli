3ad0b7aa9e21a3315504020a4c8a972f68c20ad8
luckapple <shenglgapple.@163.com>
2020-08-27 09:50:12 +0800

    修复vue3.0不能执行路由的问题

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <router-link to="/foo">Go to Foo</router-link>
-    <router-link to="/bar">Go to Bar</router-link>
+    <div>
+      <router-link to="/foo">Go to Foo</router-link>
+      <router-link to="/bar">Go to Bar</router-link>
+    </div>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
   export default {
-    computed:{
-      username(){
+    computed: {
+      username() {
         return this.$route.params.username
       }
     },
-    methods:{
-      goBack(){
-        window.history.length > 1 ? this.$router.go(-1):this.$router.push("/")
+    methods: {
+      goBack() {
+        window.history.length > 1 ? this.$router.go(-1) : this.$router.push("/")
       }
     }
   }
--- src/main.js
+++ src/main.js
@@ -9,13 +9,13 @@ Vue.use(VueRouter)
 const Foo = { template: '<div>foo</div>' }
 const Bar = { template: '<div>bar</div>' }
 
-const routes=[
-  {path:'/foo/:id',compoent:Foo},
-  {path:'/bar',compoent:Bar}
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
 })
 
 new Vue({
